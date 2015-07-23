7db9aa2a6ab02648f24dd357b7b7260b783f5cc0
Samuel Gladstone <gladston@umich.edu>
2015-07-23 19:57:35 -0400

    Add heroesService

---
+++ src/AngularJS101/Scripts/Services/heroesService.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    var heroesService = angular.module('heroesService', ['ngResource']);
+    heroesService.factory('Heroes', ['$resource',
+        function ($resource) {
+            return $resource('/api/heroes', {}, {
+                query: { method: 'GET', params: {}, isArray: true }
+            });
+        }
+    ]);
+})();
