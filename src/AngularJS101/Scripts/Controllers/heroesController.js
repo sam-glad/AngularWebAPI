20b60074c41772330ce7cedfeba5120c68125c4a
Samuel Gladstone <gladston@umich.edu>
2015-07-23 19:50:50 -0400

    Add app.js content, Heroes controller

---
+++ src/AngularJS101/Scripts/Controllers/heroesController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('heroesApp')
+        .controller('heroesController', heroesController);
+
+    heroesController.$inject = ['$scope', 'Heroes']; 
+
+    function heroesController($scope, Heroes) {
+        $scope.Heroes = Heroes.query();
+    }
+})();
--- src/AngularJS101/Scripts/app.js
+++ src/AngularJS101/Scripts/app.js
@@ -1,1 +1,7 @@
-﻿
+﻿(function () {
+    'use strict';
+    
+    angular.module('heroesApp', [
+        'heroesService'
+    ]);
+})();
