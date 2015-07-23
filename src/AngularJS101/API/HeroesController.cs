fae5f6e5a9099aa719fdaf8d2414adc61083d28e
Samuel Gladstone <gladston@umich.edu>
2015-07-23 19:42:32 -0400

    Add Heroes controller

---
+++ src/AngularJS101/API/HeroesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using AngularJS101.Models;
+
+namespace AngularJS101.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class HeroesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<DotaHero> Get()
+        {
+            HeroManager Hm = new HeroManager();
+            return Hm.GetAll;
+        }
+
+        // GET api/values/7
+        [HttpGet("{id}")]
+        public DotaHero Get(int id)
+        {
+            HeroManager HM = new HeroManager();
+            return HM.GetHeroByID(id);
+        }
+    }
+}
