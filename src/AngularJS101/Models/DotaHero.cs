c7a581bc33a007bb6b09fce416f9dcdb6a108f5c
Samuel Gladstone <gladston@umich.edu>
2015-07-23 19:34:53 -0400

    Add first model (DOTA Hero)

---
+++ src/AngularJS101/Models/DotaHero.cs
@@ -0,0 +1,9 @@
+﻿namespace AngularJS101.Models
+{
+    public class DotaHero
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
